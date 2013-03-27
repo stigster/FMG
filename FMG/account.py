8c59c0b8813f76a9729f8b46fb36c3e97dd4f1af
stigster <stigand@gmail.com>
2013-03-27 13:45:20 +0100

    Downloading mail, needs recursive func to process correctly.

--- FMG/account.py
+++ FMG/account.py
@@ -62,6 +62,9 @@ class Account():
     basename = None
     fmgdir = None
     accountdir = None
+    
+    currentMailbox = None # Used to track mailbox processing
+    currentMailboxHasChildren = None # Used to track mailbox processing
 
     maildirdir = None
     maildirnew = None
@@ -157,66 +160,178 @@ class Account():
 
         return
 
-    def parse_mailboxlist(self, line):
+### EMAIL GRABBING ###
+    def connectImap(self):
+        # Connect to IMAP Server
+        if not self.ssl: # ... without SSL
+            logger.debug("Connecting without SSL")
+            try:
+                imap_connection = imaplib.IMAP4(self.serverurl, self.port)
+            except Exception as e:
+                logger.error("Could not connect to IMAP server")
+                logger.debug(e)
+                return None
+        else: # ... or with SSL
+            logger.debug("Connecting with SSL")
+            try:
+                imap_connection = imaplib.IMAP4_SSL(self.serverurl, self.port)
+            except Exception as e:
+                logger.error("Could not connect to IMAP SSL server")
+                logger.debug(e)
+                return None
+
+        # Log in using provided username and password
+        try:
+            logger.debug("Logging in user '%s'", self.username)
+            imap_connection.login(self.username, self.password)
+        except Exception as e:
+            logger.error("Could not log on to the user account '%s'", self.username)
+            logger.debug(e)
+            return None
+        
+        return imap_connection        
+
+    def parse_mailboxlist(self, mailbox_list_item):
+        """Parse the IMAP account list of mailboxes/folders."""
+        logger.debug("Parsing mailbox list item '%s'", mailbox_list_item)
         list_response_pattern = re.compile(r'\((?P<flags>.*?)\) "(?P<delimiter>.*)" (?P<name>.*)')
         
-        flags, delimiter, mailbox_name = list_response_pattern.match(line).groups()
+        flags, delimiter, mailbox_name = list_response_pattern.match(mailbox_list_item).groups()
         mailbox_name = mailbox_name.strip('"')
         return (flags, delimiter, mailbox_name)
          
+    def parse_raw_email(self, raw_email):
+        """Parse raw email messages."""
+        maintype = raw_email.get_content_maintype()
+        if maintype == 'multipart':
+            for part in raw_email.get_payload():
+                if part.get_content_maintype() == 'text':
+                    return part.get_payload()
+        elif maintype == 'text':
+            return raw_email.get_payload()
+        
+    def email_to_txt(self, email_id, email_message, mailbox_path):
+        # Get the basic parts of the email message
+        if 'message-id' in email_message:
+            email_message_id = email_message['message-id']
+        email_message_from = email_message['From']
+        email_message_to = email_message['To']
+        email_message_subject = email_message['Subject']
+        email_message_text = self.parse_raw_email(email_message) # Extract the text-part of the message
+        
+        # Write the email to a text file
+        email_filename = email_id + ".txt"
+        try:
+            logger.debug("Writing email to text file '%s'", os.path.join(mailbox_path, email_filename))
+            f = open(os.path.join(mailbox_path, email_filename), 'w')
+            if'message-id' in email_message:
+                f.write("ID: " + email_message_id + "\n")
+            f.write("From: " + email_message_from + "\n")
+            f.write("To: " + email_message_to + "\n")
+            f.write("Subject: " + email_message_subject + "\n")
+            f.write(email_message_text)
+            f.close()
+        except Exception as e:
+            logger.warn("Failed to write email to text file '%s'", os.path.join(mailbox_path, email_filename))
+            logger.debug(e)
+        return
+    
+    def processMailbox(self, imap_connection, mailbox_name):
+        # TODO: Create MBOX
+       
+        # Create folder for this mailbox
+        mailbox_path = os.path.join(self.accountdir, self.currentMailbox)
+        if not os.path.exists(mailbox_path):
+            try:
+                logger.debug("Creating mailbox folder '%s'", mailbox_path)
+                os.mkdir(mailbox_path)
+            except Exception as e:
+                logger.warn("Could not create mailbox folder")
+                logger.debug(e)        
 
-### EMAIL GRABBING ###
+        # Get a list of all the messages in the selected mailbox
+        s_result, s_data = imap_connection.search(None, "ALL")
+        logger.debug("Mailbox search result: %s", s_result)
+        logger.debug("Mailbox search data: %s", s_data)
+        for email_id in s_data[0].split():
+            logger.debug("Fetching mail #%s", email_id)
+            f_result, f_data = imap_connection.fetch(email_id, "(RFC822)") # Fetch the complete email message
+            logger.debug("Mailbox fetch result: %s", f_result)
+            email_message = email.message_from_string(f_data[0][1])
+            
+            # TODO: Add the email  message to MBOX
+            
+            # Write the email to a TXT file
+            self.email_to_txt(email_id, email_message, mailbox_path)
+            
+        # Close selected mailbox
+        try:
+            logger.debug("Closing selected IMAP mailbox")
+            imap_connection.close()
+        except Exception as e:
+                logger.warn("Could not close selected IMAP mailbox")
+                logger.debug(e)
+
+        return
+    
     def grabImap(self):
         """Grabs mail from the accounts IMAP server"""
         logger.debug("Grabbing mail from IMAP server")
 
-        # Connect and log in
-        if not self.ssl:
-            logger.debug("Connecting without SSL")
-            try:
-                mailbox = imaplib.IMAP4(self.serverurl, self.port)
-            except Exception as e:
-                logger.error("Could not connect to IMAP server")
-                logger.debug(e)
-                return
-        else:
-            logger.debug("Connecting with SSL")
-            try:
-                mailbox = imaplib.IMAP4_SSL(self.serverurl, self.port)
-            except Exception as e:
-                logger.error("Could not connect to IMAP SSL server")
-                logger.debug(e)
-                return
-
-        logger.debug("Logging in")
-        try:
-            mailbox.login(self.username, self.password)
-        except Exception as e:
-            logger.error("Could not log on to the account")
-            logger.debug(e)
-            return
-
-        mailbox_list = mailbox.list()
+        # Connect to IMAP Server
+        # TODO: Add try - except block
+        imap_connection = self.connectImap()
         
-        print mailbox_list
-
-#        mailbox.select()
-#        typ, data = mailbox.search(None, 'ALL')
-#        for num in data[0].split():
-#            typ, data = mailbox.fetch(num, '(RFC822)')
-#            print 'Message %s\n%s\n' % (num, data[0][1])
-
-        # Close selected mailbox and log out
-        logger.debug("Closing selected mailbox")
+        # Get all the mailboxes/folders in the IMAP account
         try:
-            mailbox.close()
+            logger.debug("Getting IMAP mailbox list")
+            response_code, mailbox_list = imap_connection.list()
+            logger.debug(mailbox_list)
         except Exception as e:
-                logger.warn("Could not close selected mailbox")
-                logger.debug(e)
+            logger.critical("Could not list IMAP mailboxes")
+            logger.debug(e)
+            logger.debug("Logging out")
+        
+        # For each mailbox/folder, grab all email messages
+        if response_code == 'OK':
+            logger.debug("Mailbox processing...")
+            self.currentMailbox = "" 
+            
+            for mailbox_list_item in mailbox_list:
+                flags, delimiter, mailbox_name = self.parse_mailboxlist(mailbox_list_item)
 
-        logger.debug("Logging out")
+                # Select the mailbox. (If selection fails, return and continue with the next mailbox.)
+                try:
+                    logger.debug("Selecting IMAP mailbox '%s'", mailbox_name)
+                    imap_connection.select(mailbox_name)
+                except Exception as e:
+                    logger.warn("Failed to select IMAP Mailbox '%s'", mailbox_name)
+                    logger.debug(e)
+                    continue
+
+                # If the current mailbox has children, processing must be repeated for all children (recursive processing).
+                if re.search("HasChildren", flags):
+                    logger.debug("Mailbox has children")
+                    response_code, childlist = imap_connection.list(mailbox_name)
+                    logger.debug("(%s) %s", response_code, childlist)
+                    for child in childlist:
+                        child_flag, child_delimiter, child_name = self.parse_mailboxlist(child)
+                        logger.debug("Adding '%s' to list of mailboxes to process", child_name)
+                        mailbox_list.append(child_name)
+                else:
+                    logger.debug("Mailbox has no children")
+                    try:
+                        self.currentMailbox = os.path.join(self.currentMailbox, mailbox_name)
+                        self.processMailbox(imap_connection, mailbox_name)
+                    except Exception as e:
+                        logger.warn("Could not process IMAP mailbox '%s'", mailbox_name)
+                        logger.debug(e)
+                        return
+
+        # Log out from IMAP connection
         try:
-            mailbox.logout()
+            logger.debug("Logging out from IMAP Connection")
+            imap_connection.logout()
         except Exception as e:
             logger.warn("Could not log out from server")
             logger.debug(e)
